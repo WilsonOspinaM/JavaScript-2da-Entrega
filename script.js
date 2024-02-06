1382f2432e90fe640a73e18cdb496b44e041b5f7
Wilson Ospina <89227182+WilsonOspinaM@users.noreply.github.com>
2024-02-05 23:25:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,96 @@
 const servicios = [
-    {id:1, nombre:"Logotipo", precio:157900, tiempoEntrega:10},
-    {id:2, nombre:"Slogan", precio:42800, tiempoEntrega:4},
-    {id:3, nombre:"Claim", precio:42800, tiempoEntrega:6},
-    {id:4, nombre:"Tarjetas personales", precio:27200, tiempoEntrega:2},
-    {id:5, nombre:"Hojas membreteadas", precio:27200, tiempoEntrega:3},
-    {id:6, nombre:"Carpeta empresarial", precio:59800, tiempoEntrega:5},
-    {id:7, nombre:"Postal", precio:46800, tiempoEntrega:2},
-    {id:8, nombre:"Volante", precio:41600, tiempoEntrega:4},
-    {id:9, nombre:"Díptico", precio:89400, tiempoEntrega:8},
-]
-
-let Categoria = prompt('Cuantos empleados tiene tu empresa')
-
-console.log(servicios[2]);
+  { id: 1, nombre: "Logotipo", precio: 157900, tiempoEntrega: 10 },
+  { id: 2, nombre: "Slogan", precio: 42800, tiempoEntrega: 4 },
+  { id: 3, nombre: "Claim", precio: 42800, tiempoEntrega: 6 },
+  { id: 4, nombre: "Tarjetas personales", precio: 27200, tiempoEntrega: 2 },
+  { id: 5, nombre: "Hojas membreteadas", precio: 27200, tiempoEntrega: 3 },
+  { id: 6, nombre: "Carpeta empresarial", precio: 59800, tiempoEntrega: 5 },
+  { id: 7, nombre: "Postal", precio: 46800, tiempoEntrega: 2 },
+  { id: 8, nombre: "Volante", precio: 41600, tiempoEntrega: 4 },
+  { id: 9, nombre: "Díptico", precio: 89400, tiempoEntrega: 8 },
+];
 
 class Pedido {
-    constructor(){
-        this.servicios = [];
-        this.totalCompra = 0;
-        this.CategoriaA = 35;
+  constructor() {
+    this.servicios = [];
+    this.totalCompra = 0;
+    this.descuento = 3;
+  }
 
+  agregarServicio(id) {
+    let servicio = servicios.find((serv) => serv.id === id);
+    if (servicio) {
+      this.servicios.push(servicio);
+      console.log("Agregaste el servicio " + id + " a tu proyecto");
+    } else {
+      console.log("No agregaste productos a tu proyecto");
     }
+  }
 
-    agregarServicio(id){
-let servicio = servicios.find(serv => serv.id === id);
-if (servicio) {
-    this.servicios.push(servicio);
-    console.log("Agregaste el servicio " + id + " a tu proyecto");
-} else {
-    console.log("No agregaste productos a tu proyecto");
-}
+  totalServicios() {
+    return this.servicios.length;
+  }
 
-    }
-
-    listarPedido(){
-this.servicios.forEach(item => {
-    console.log(item.id + ". " + item.nombre + " $" + item.precio + " con entrega para " + item.tiempoEntrega + " días");
-})
-    }
-
-    totalServicios(){
-        return this.servicios.length;
-    }
-
-    totalSumatoriaServicios(){
+  totalSumatoriaServicios() {
     let total = 0;
-        this.servicios.forEach(item => {
-            total += item.precio;
-        });
+    this.servicios.forEach((item) => {
+      total += item.precio;
+    });
 
-        return total;
+    return total;
+  }
+
+  aplicarDescuento() {
+    if (this.totalServicios() >= this.descuento) {
+      return Math.round((this.totalPedido() * this.descuento) / 100);
     }
+  }
 
-    aplicarCategoria(){
-        if (this.Categoria()=="A") {
-            return Math.round(this.totalCompra + ((this.totalCompra * this.CategoriaA) / 100));
-        } 
-    }
+  totalPedido() {
+    let total = 0;
 
-    totalPedido(){
-        this.servicios.forEach(item => {
-            this.totalCompra += item.precio;
-        });
+    this.servicios.forEach((item) => {
+      total += item.precio;
+    });
 
-        return this.totalCompra;
-    }
+    return total;
+  }
 }
- 
+
+function listarPedido() {
+  let lista = "";
+  servicios.forEach((item) => {
+    lista +=
+      item.id +
+      ". " +
+      item.nombre +
+      " $" +
+      item.precio +
+      " con entrega para " +
+      item.tiempoEntrega +
+      " días" +
+      "\n";
+  });
+  return lista;
+}
+
 const pedido = new Pedido();
-pedido.agregarServicio(1);
-pedido.agregarServicio(3);
-pedido.agregarServicio(6);
-pedido.agregarServicio(7);
-pedido.listarPedido();
+let servicioSeleccionado = 10;
 
-console.log("Total servicios del proyecto: " + pedido.totalServicios());
-let totalProyecto = totalSumatoriaServicios();
-console.log("Total para tu proyecto es de $" + totalProyecto);
+while (servicioSeleccionado != 0) {
+  servicioSeleccionado = parseInt(
+    prompt( "Selecciona el servicio que desees (Coloca 0 para finalizar)\n\n" + listarPedido()
+    )
+  );
 
-const tipoCliente = [
-    {id:1, tipo:"A", percent:35},
-    {id:2, tipo:"B", percent:30},
-]
+  if (servicioSeleccionado == 0) {
+    break;
+  }
+
+  pedido.agregarServicio(servicioSeleccionado);
+}
+
+let totalPedido = pedido.totalSumatoriaServicios();
+let descuento = pedido.aplicarDescuento();
+let valorFinal = totalPedido - descuento;
+alert("El total de tu proyecto es $" + valorFinal);
